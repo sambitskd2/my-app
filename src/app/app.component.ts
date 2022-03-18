559edbd83adb4336462820b55f0ee985862c4e50
Sambitskd2 <sambitskd2@gmail.com>
2022-03-18 18:14:37 +0530

    post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  language: string = '';
+  listOfLanguages: string[] = ['English', 'Hidi'];
+
+  addLanguage() {
+    this.listOfLanguages.push(this.language);
+    console.log(this.listOfLanguages);
+  }
+  deleteFirst() {
+    this.listOfLanguages.splice(0, 1);
+  }
 }
